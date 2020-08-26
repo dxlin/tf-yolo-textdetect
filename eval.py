67ae26ec772efaf56391fd9ce30d094d285c70fb
Daniel Xun Lin <danielx.lin@gmail.com>
2020-08-25 23:00:20 -0400

    -cleanup basic eval code a bit

--- eval.py
+++ eval.py
@@ -24,11 +24,6 @@ info = 5
 grid_w = 16
 grid_h = 16
 
-def save_model(model):
-    model_json = model.to_json()
-    with open("model1/text_detect_model.json", "w") as json_file:
-        json_file.write(model_json)
-
 def load_model(strr):        
     json_file = open(strr, 'r')
     loaded_model_json = json_file.read()
@@ -36,87 +31,6 @@ def load_model(strr):
     loaded_model = model_from_json(loaded_model_json)
     return loaded_model
     
-def yolo_model(input_shape):
-    
-    
-    inp = Input(input_shape)
-   
-    model = MobileNetV2( input_tensor= inp , include_top=False, weights='imagenet')
-    last_layer = model.output
-    
-    conv = Conv2D(512,(3,3) , activation='relu' , padding='same')(last_layer)
-    conv = Dropout(0.4)(conv)
-    bn = BatchNormalization()(conv)
-    lr = LeakyReLU(alpha=0.1)(bn)
-    
-    
-    conv = Conv2D(128,(3,3) , activation='relu' , padding='same')(lr)
-    conv = Dropout(0.4)(conv)
-    bn = BatchNormalization()(conv)
-    lr = LeakyReLU(alpha=0.1)(bn)
-    
-    
-    conv = Conv2D(5,(3,3) , activation='relu' , padding='same')(lr)
-    
-    final = Reshape((grid_h,grid_w,classes,info))(conv)
-    
-    model = Model(inp,final)
-    
-    return model
-
-#define utils
-
-#optimizer
-opt = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-
-#checkpoint
-checkpoint = ModelCheckpoint('model1/text_detect.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='min',period=1)
-
-#define loss function
-def yolo_loss_func(y_true,y_pred):
-    # y_true : 16,16,1,5
-    # y_pred : 16,16,1,5
-    l_coords = 5.0
-    l_noob = 0.5
-    coords = y_true[:,:,:,:,0] * l_coords
-    noobs = (-1*(y_true[:,:,:,:,0] - 1)*l_noob)
-    p_pred = y_pred[:,:,:,:,0]
-    p_true = y_true[:,:,:,:,0]
-    x_true = y_true[:,:,:,:,1]
-    x_pred = y_pred[:,:,:,:,1]
-    yy_true = y_true[:,:,:,:,2]
-    yy_pred = y_pred[:,:,:,:,2]
-    w_true = y_true[:,:,:,:,3]
-    w_pred = y_pred[:,:,:,:,3]
-    h_true = y_true[:,:,:,:,4]
-    h_pred = y_pred[:,:,:,:,4]
-    
-    p_loss_absent = K.sum(K.square(p_pred - p_true)*noobs)
-    p_loss_present = K.sum(K.square(p_pred - p_true))
-    x_loss = K.sum(K.square(x_pred - x_true)*coords)
-    yy_loss = K.sum(K.square(yy_pred - yy_true)*coords)
-    xy_loss = x_loss + yy_loss
-    w_loss = K.sum(K.square(K.sqrt(w_pred) - K.sqrt(w_true))*coords)
-    h_loss = K.sum(K.square(K.sqrt(h_pred) - K.sqrt(h_true))*coords)
-    wh_loss = w_loss + h_loss
-    
-    loss = p_loss_absent + p_loss_present + xy_loss + wh_loss
-    
-    return loss
-
-#load and save model
-input_size = (img_h,img_w,channels)
-
-#model = yolo_model(input_size)
-#
-#print(model.summary())
-#
-#save_model(model)
-#
-#model.load_weights('model/text_detect.h5')
-#
-#model.compile(loss=yolo_loss_func , optimizer=opt , metrics = ['accuracy'])
-
 model = load_model('model/text_detect_model.json')
 model.load_weights('model/text_detect.h5')
 
